d5e04ed4fc3a6e57fe83f055003ce7eadafcaa6a
Harish Chandrasekaran <harish.chandrasekaran@s759labs.com>
2021-06-08 08:43:37 +0530

    Backend initialised

---
+++ src/main/java/com/covistats/api/CovistatsApplication.java
@@ -0,0 +1,13 @@
+package com.covistats.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovistatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovistatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covistats/api/CovistatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covistats.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovistatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
